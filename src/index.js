38558ba6055ef36bdc6e3f03246c2748a08b0519
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2021-08-07 14:28:38 +0300

    features/add menu markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './sass/main.scss';
+import menuItemTpl from './templates/templateMenuItems.hbs';
+import menu from './menu.json';
+import refs from './js/refs.js';
+
+function renderMarkup() {
+    return refs.menu.insertAdjacentHTML('beforeend', menuItemTpl(menu))
+}
+
+renderMarkup();
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default
+    {
+        menu: document.querySelector('.js-menu'),
+        themeSwitcher: document.querySelector('.theme-switch__toggle'),
+
+    };

ee7faed6825f27cdfb3c2dd91dd0215ff2cfce78
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2021-08-07 18:09:22 +0300

    add theme Switcher

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import menuItemTpl from './templates/templateMenuItems.hbs';
 import menu from './menu.json';
 import refs from './js/refs.js';
+import Theme from './js/theme.js';
+
+refs.themeSwitcher.addEventListener('change', onChange);
+window.addEventListener('load',checkTheme);
 
 function renderMarkup() {
     return refs.menu.insertAdjacentHTML('beforeend', menuItemTpl(menu))
 }
 
 renderMarkup();
+
+refs.themeSwitcher.checked = false;
+
+function onChange() {
+    if (refs.themeSwitcher.checked) {
+        refs.body.classList.remove('light-theme');
+        refs.body.classList.add('dark-theme');
+        localStorage.setItem('Theme', JSON.stringify(Theme.DARK));
+    } else {
+        refs.body.classList.remove('dark-theme');
+        refs.body.classList.add('light-theme');
+        localStorage.setItem('Theme', JSON.stringify(Theme.LIGHT));
+    }
+}
+
+function checkTheme() {
+    const localStorageTheme = localStorage.getItem('Theme');
+    console.log(localStorageTheme)
+    if (localStorageTheme !== null && localStorageTheme === JSON.stringify(Theme.DARK)) {
+        refs.body.classList.add(localStorageTheme);
+        refs.themeSwitcher.checked = true;
+    }
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,5 +2,6 @@ export default
     {
         menu: document.querySelector('.js-menu'),
         themeSwitcher: document.querySelector('.theme-switch__toggle'),
+        body: document.querySelector('body'),
 
     };
